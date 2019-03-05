825cb0525276bca3d163a1abf733d9e346c4632c
黃哲芬 <annie101101046@gmail.com>
2019-03-05 15:03:45 +0800

    LintRevised

--- profile.js
+++ profile.js
@@ -1,55 +1,61 @@
-//facebook initialize
-//fb 的元件初始化
+/* eslint-disable no-shadow */
+/* eslint-disable prefer-const */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-undef */
+// facebook initialize
+// fb 的元件初始化
 
-function checkLogin(){
-	FB.getLoginStatus(function(response) {
-	 var fbButton = document.querySelector('.fb-login-button');
-	 var loadingImg = document.createElement('img');
-	 var header = document.querySelector('.header');
-	 //在 callback function 拿到資料後就會被覆寫
-	 header.style.backgroundImage = "url(img/loading.gif)";
-	 loadingImg.setAttribute("class", "loadingImg");
+function checkLogin() {
+  FB.getLoginStatus((response) => {
+	 const fbButton = document.querySelector('.fb-login-button');
+	 const loadingImg = document.createElement('img');
+	 const header = document.querySelector('.header');
+	 // 在 callback function 拿到資料後就會被覆寫
+	 header.style.backgroundImage = 'url(img/loading.gif)';
+	 loadingImg.setAttribute('class', 'loadingImg');
 	 if (response.status === 'connected') {
 		 statusChangeCallback(response);
-		 loadingImg.style.display = "none";
-	 } 
-	});
+		 loadingImg.style.display = 'none';
+	 }
+  });
 }
 
-	window.fbAsyncInit = function() {
-		
+window.fbAsyncInit = function () {
 		    FB.init({
-		      appId      : '157112091854354',
-		      cookie     : true,
-		      xfbml      : true,
-		      version    : 'v3.2'
+		      appId: '157112091854354',
+		      cookie: true,
+		      xfbml: true,
+		      version: 'v3.2',
 		    });
-		      
 		    FB.AppEvents.logPageView();
 		    checkLogin();
 		  };
-//在整個網頁 load 完以後，才把 FB init 起來，才做 getLoginStatus
+// 在整個網頁 load 完以後，才把 FB init 起來，才做 getLoginStatus
 // 		    FB.login(function(response) {
 // 		    console.log(response);
 //    			statusChangeCallback(response);
 
 // }, {scope: 'public_profile,email'});
 
-//初始化完成以後，要判斷有沒有 log in
+// 初始化完成以後，要判斷有沒有 log in
 
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
+		  (function (d, s, id) {
+		     let js; const
+    fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) { return; }
 		     js = d.createElement(s); js.id = id;
-		     js.src = "https://connect.facebook.net/en_US/sdk.js";
+		     js.src = 'https://connect.facebook.net/en_US/sdk.js';
 		     fjs.parentNode.insertBefore(js, fjs);
 		   }(document, 'script', 'facebook-jssdk'));
 
-		 //facebook會在網頁load進來後拿到這些 response
-		 //已判斷是不是已經 login 
-		 //如果沒 login ，就再去拿 FB login 的 function
+		 // facebook會在網頁load進來後拿到這些 response
+		 // 已判斷是不是已經 login
+		 // 如果沒 login ，就再去拿 FB login 的 function
 
-//response 給的資料
+// response 給的資料
 // {
 //     status: 'connected',
 //     authResponse: {
@@ -62,31 +68,28 @@ function checkLogin(){
 // }
 
 
-//如果 status 沒有 connected，就要去拿 FB login 的 function
-//statusChangeCallback(response)在上面被 call 過了，拿到了 accessToken，FB 會幫處理
-function statusChangeCallback(response){	
+// 如果 status 沒有 connected，就要去拿 FB login 的 function
+// statusChangeCallback(response)在上面被 call 過了，拿到了 accessToken，FB 會幫處理
+function statusChangeCallback(response) {
+  FB.api(
+    // https://developers.facebook.com/docs/graph-api/reference/user
+    '/me', // 直接拿 login 這個人的資料
+    'GET',
+    { fields: 'picture.type(large), email, name' },
+    (response) => {
+      const header = document.querySelector('.header');
+      header.style.backgroundImage = `url('${response.picture.data.url}')`;
 
-FB.api(
-//https://developers.facebook.com/docs/graph-api/reference/user
-	'/me', //直接拿 login 這個人的資料
-	'GET',
-	{"fields": "picture.type(large), email, name"},
-	function(response) {
-		var header = document.querySelector('.header');
-		header.style.backgroundImage = `url('${response['picture']['data']['url']}')`;
-			
-		var checkmark = document.querySelector('.checkmark');
-		checkmark.innerHTML = '姓名： ' + response['name'];
-		var crossmark = document.querySelector('.crossmark');
-		crossmark.innerHTML = 'Email： ' + response['email'];
-		// Insert your code here
-	});
+      const checkmark = document.querySelector('.checkmark');
+      checkmark.innerHTML = `姓名： ${response.name}`;
+      const crossmark = document.querySelector('.crossmark');
+      crossmark.innerHTML = `Email： ${response.email}`;
+      // Insert your code here
+    },
+  );
 }
-	//reply 回來的樣子
-	// {
- //  "name": "Your Name",
- //  "id": "your-user-id"
-	// }
-
-
-
+// reply 回來的樣子
+// {
+//  "name": "Your Name",
+//  "id": "your-user-id"
+// }
--- thankyou.js
+++ thankyou.js
@@ -1,20 +1,15 @@
 const querytString = window.location.search;
-//slice 只拿這個位置之後的字串 -->>(去找 = 在第幾個位子 + 1)
-let orderNum = querytString.slice(querytString.indexOf('=') + 1);
+// slice 只拿這個位置之後的字串 -->>(去找 = 在第幾個位子 + 1)
+const orderNum = querytString.slice(querytString.indexOf('=') + 1);
 
-//宣告一個 function getProductDetails 是要找 /products/details?id=${id}
-//call get 這個 function
+// 宣告一個 function getProductDetails 是要找 /products/details?id=${id}
+// call get 這個 function
 
 const printOrderNumber = (data) => {
-        var header = document.querySelector('.header');
-        var checkmark = document.querySelector('.checkmark');
-        header.innerHTML = "感謝您的訂購：）";
-        checkmark.innerHTML = "您的訂單編號是：" + data ;
-}
+  const header = document.querySelector('.header');
+  const checkmark = document.querySelector('.checkmark');
+  header.innerHTML = '感謝您的訂購：）';
+  checkmark.innerHTML = `您的訂單編號是：${data}`;
+};
 
 printOrderNumber(orderNum);
-
-
-
-
-
